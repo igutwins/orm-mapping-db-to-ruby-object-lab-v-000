027568d3e5b489a01e79669ccc1b1a01f3fdc36c
Ian Gutwinski <iangutwinski@gmail.com>
2020-01-29 03:19:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,8 +71,8 @@ class Student
 
       DB[:conn].execute(sql).map do |row|
         self.new_from_db(row)
-      end
-  end.first
+      end.first
+  end
 
   def self.find_by_name(name)
     # find the student in the database given a name
