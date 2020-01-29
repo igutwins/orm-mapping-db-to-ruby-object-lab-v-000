9a9fb2a345485f410de3da8c4712343a11236f0c
Ian Gutwinski <iangutwinski@gmail.com>
2020-01-29 03:14:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,6 +23,21 @@ class Student
     end
   end
 
+  def self.all_students_in_grade_9
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+      SQL
+
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end.first
+  end
+
+
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
